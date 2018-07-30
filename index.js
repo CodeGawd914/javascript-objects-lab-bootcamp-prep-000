2e6829ad0669865e8e0a2706d6c0d18fc74203b8
travisgdesir31@gmail.com <travisgdesir31@gmail.com>
2018-07-30 03:15:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ function deleteFromObjectByKey(object,key){
  return newobj
 }
 function destructivelyDeleteFromObjectByKey(object,key){
-  delete object.key
+  delete object[key]
   return object
 }
